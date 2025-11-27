874fd4e9a87a2081e58b65b09cf1823392227bc2
KedarA45 <adakekedar45@gmail.com>
2025-11-27 17:24:48 +0530

    Updated Sequence

--- Tesebench/lift_sequence.sv
+++ Tesebench/lift_sequence.sv
@@ -1,8 +1,9 @@
-class lift_sequence extends uvm_sequence;
+class lift_sequence extends uvm_sequence#(lift_sequence_item);
     /* In this class we start item randomize it and finish it  */
 
     /* Factory Registration  */
     `uvm_object_utils(lift_sequence)
+    lift_sequence_item req;
 
     /* New Function */
     function new(string name = "lift_sequence");
@@ -11,6 +12,17 @@ class lift_sequence extends uvm_sequence;
 
     task body;
         /* About Task Body: */
+        repeat(10) begin
+            req = lift_sequence_item::type_id::create("req");
+            start_item(req);
+               if(!req.randomize()) begin
+                   `uvm_info(get_type_name,"SEQ ITEM NOT RANDOMIZED",UVM_NONE)
+               end
+               else begin
+                   `uvm_info(get_type_name,"SEQ ITEM IS RANDOMIZED SUCCESSFULLY",UVM_NONE)
+               end
+            finish_item(req);
+        end
     endtask
 endclass
 /* lift_sequence class ends */
