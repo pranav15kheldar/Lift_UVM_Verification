cdeef1a77ee95512c3d756940cec1496ad9a719d
KedarA45 <adakekedar45@gmail.com>
2025-11-27 17:25:13 +0530

    Updated Driver

--- Tesebench/lift_driver.sv
+++ Tesebench/lift_driver.sv
@@ -1,8 +1,9 @@
-class lift_driver extends uvm_driver;
+class lift_driver extends uvm_driver#(lift_sequence_item);
     /* About lift_driver : */
 
     /* Factory registration */
     `uvm_component_utils(lift_driver)
+    virtual lift_interface vif;
 
     /* New FUnction */
     function new(string name = "lift_driver", uvm_component parent = null );
@@ -13,6 +14,13 @@ class lift_driver extends uvm_driver;
     function void build_phase(uvm_phase phase);
         super.build_phase(phase);
         `uvm_info(get_type_name(),"BUILD PHASE",UVM_NONE)
+        req = lift_sequence_item::type_id::create("req");
+        if(!uvm_config_db#(virtual lift_interface)::get(this," ","vif",vif)) begin
+            `uvm_info(get_type_name(),"DID NOT GET INTERFACE",UVM_NONE)
+        end
+        else begin
+            `uvm_info(get_type_name(),"G0T INTERFACE!!",UVM_NONE)
+        end
     endfunction
 
     /* Connect phase */
@@ -36,6 +44,14 @@ class lift_driver extends uvm_driver;
     /* Run Phase  */
     task run_phase(uvm_phase phase);
         super.run_phase(phase);
+        forever begin
+            seq_item_port.get_next_item(req);
+            req.print();
+            @(posedge vif.i_clk);
+            reset();
+            perform_txn(req.i_up, req.i_down, req.i_floor,req.i_open, req.i_close,req.i_power,req.i_battery,req.i_weight);
+            seq_item_port.item_done();
+        end
     endtask
 
     /* Extract Phase */
@@ -61,6 +77,29 @@ class lift_driver extends uvm_driver;
         super.final_phase(phase);
         `uvm_info(get_type_name(),"FINAL PHASE",UVM_NONE)
     endfunction
+    
+    task reset();
+         vif.i_up     <= 0;    
+         vif.i_down   <= 0;
+         vif.i_floor  <= 0;
+         vif.i_open   <= 0;
+         vif.i_close  <= 0;
+         vif.i_power  <= 0;
+         vif.i_battery<= 0;
+         vif.i_weight <= 0;
+         repeat(5) @(posedge vif.i_clk);
+    endtask
+
+    task perform_txn(logic [4:0]i_up, i_down, i_floor,logic i_open, i_close,i_power,i_battery,i_weight);
+         vif.i_up      <= i_up     ; 
+         vif.i_down    <= i_down   ;
+         vif.i_floor   <= i_floor  ;
+         vif.i_open    <= i_open   ;
+         vif.i_close   <= i_close  ;
+         vif.i_power   <= i_power  ;
+         vif.i_battery <= i_battery;
+         vif.i_weight  <= i_weight ;
+    endtask
 
 endclass
 /* End of lift_driver Class */
