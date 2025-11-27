472d54f025d322cbef07c06cb58a315c7e1802dd
KedarA45 <adakekedar45@gmail.com>
2025-11-27 17:32:17 +0530

    added signals

--- Tesebench/lift_interface.sv
+++ Tesebench/lift_interface.sv
@@ -1,3 +1,13 @@
-interface lift_interface(input clk);
+interface lift_interface(input i_clk);
+  logic  [4:0]  i_up, i_down;
+  logic  [4:0]  i_floor;
+  logic         i_open;
+  logic         i_close;
+  logic         i_power;
+  logic         i_battery;
+  logic         i_weight;
+  logic  [4:0]  o_final_pos;
+  logic         o_open;
+  logic         o_close;
     
 endinterface
